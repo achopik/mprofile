1879939fb9a7c1cfb78d196edf93d95e344fa82b
achopik <artikchopik@gmail.com>
2025-06-10 13:45:06 +0300

    fix: arm64 builds

--- docker_build_wheels.sh
+++ docker_build_wheels.sh
@@ -1,6 +1,50 @@
 #!/bin/bash
-
 set -exuo pipefail
 
-python3 setup.py sdist
-docker run -it -v `pwd`:/src -e PLAT=manylinux2014_x86_64 quay.io/pypa/manylinux2014_x86_64 /src/build_wheels.sh
+PLATFORMS="x86_64"
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -p|--platform)
+            PLATFORMS="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+python -m build --sdist
+
+SRC_DIR="$(pwd)"
+
+build_x86_64() {
+    docker run -i --rm \
+        -v "$SRC_DIR:/src" \
+        -e PLAT=manylinux2014_x86_64 \
+        quay.io/pypa/manylinux2014_x86_64 \
+        /src/build_wheels.sh
+}
+
+build_arm64() {
+    docker run --platform=linux/arm64 -i --rm \
+        -v "$SRC_DIR:/src" \
+        -e PLAT=manylinux2014_aarch64 \
+        quay.io/pypa/manylinux2014_aarch64 \
+        /src/build_wheels.sh
+}
+
+case "$PLATFORMS" in
+    x86_64)
+        build_x86_64
+        ;;
+    arm64)
+        build_arm64
+        ;;
+    *)
+        echo "Unknown platform: $PLATFORMS"
+        exit 1
+        ;;
+esac
--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 import fnmatch
 import glob
@@ -52,40 +50,6 @@ def get_version():
 
     raise RuntimeError("Cannot determine version from mprofile/__init__.py.")
 
-
 setup(
-    name="mprofile",
-    version=get_version(),
-    description="A low-overhead memory profiler.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    platforms=["Mac OS X", "POSIX"],
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-        "Operating System :: POSIX",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Topic :: Software Development :: Testing",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    project_urls={
-        "Source": "https://github.com/timpalpant/mprofile",
-        "Tracker": "https://github.com/timpalpant/mprofile/issues",
-    },
-    keywords="profiling performance",
-    url="http://github.com/timpalpant/mprofile",
-    author="Timothy Palpant",
-    author_email="tim@palpant.us",
-    license="MIT",
-    setup_requires=["wheel"],
-    packages=["mprofile"],
     ext_modules=[ext],
-    test_suite="test",
 )
